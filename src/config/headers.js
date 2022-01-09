4aa14cae3dacba2c866b9fbc61b53076a85eee64
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2022-01-08 19:38:33 -0800

    mixed content http - https

--- src/config/headers.js
+++ src/config/headers.js
@@ -1,4 +1,5 @@
 export const headers = {
     'Content-Type':'application/json',
-    "Access-Control-Allow-Origin": "*"
+    "Access-Control-Allow-Origin": "*",
+    "Content-Security-Policy": "upgrade-insecure-requests"
 }
