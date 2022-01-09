49fbe1442be18b71ae987686e99ac5c62fd24594
ReynaldoCerpa <reynaldocerpa07@gmail.com>
2022-01-08 19:30:00 -0800

    env var

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
     const nav = useNavigate();
 
     const handleSubmit = async (id) => {
+        console.log(process.env.REACT_APP_URL);
         setError(false)
         setAlert(false)
         setLoading(true)
